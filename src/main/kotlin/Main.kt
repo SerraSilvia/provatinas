0c57c996a3abf2f74f44a9e424d7160e94043aaf
silvia.serra.7e6 <silvia.serra.7e6@itb.cat>
2024-01-15 13:55:41 +0100

    factorail

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,27 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    println("El factorial de 4 és: ${factorial(4)}")
+    println("El doble factorial de 4 és: ${dobleFactorial(4)}")
+    bucle(10)
 }
+
+fun factorial(num:Int):Int{
+    return if (num==1)1
+    else num*factorial(num-1)
+}
+
+fun bucle(num:Int):Int{
+    if (num ==0) return 0
+    else{
+        println(num)
+        return bucle(num-1)
+    }
+
+}
+
+fun dobleFactorial(num:Int){
+    return if (num==1)1
+    else (num*num)*(dobleFactorial*dobleFactorial)(num-1)
+}
+
