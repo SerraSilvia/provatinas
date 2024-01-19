fa1f26aeef5dcae3573b6d3afde4cbe688f470b3
silvia.serra.7e6 <silvia.serra.7e6@itb.cat>
2024-01-19 08:43:42 +0100

    add fibonacci

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,9 +21,30 @@ fun dobleFactorial(num:Int):Int{
     else (num*num)*dobleFactorial(num-1)
 }
 fun nombreDigits(num:Int):Int {
-    return if (num <= 9) 1
+    return if (num <= 9) 1  //si és < o = a 9, té 1 digit
     else 1 + nombreDigits((num.toString().dropLast(1).toInt()))
+
+    // 1. Convertimos el número a una cadena de texto: num.toString()
+    // 2. Eliminamos el último dígito de la cadena: dropLast(1)
+    // 3. Convertimos la cadena resultante de nuevo a un número entero: toInt()
+    // 4. Llamamos recursivamente a la función con el nuevo número obtenido.
+
 //dropLast te permite obtener una nueva colección que contiene
 // todos los elementos de la colección original excepto
 // los últimos N elementos especificados.
+}
+fun numeroInvertit(){
+
+}
+
+fun fibonacci(num:Int):Int{
+    return if(num==0)0
+    else if(num==1)1
+    else fibonacci(num-1+ fibonacci(-2))
+}
+
+fun fibonacciAltraMAnera(num1:Int, num2:Int, iteracions:Int){
+    println("$num1+$num2=${num2+num1}")
+    if (iteracions==0)"Adéu"
+    else (fibonacciAltraMAnera(num2, num1+num2,iteracions-1))
 }
